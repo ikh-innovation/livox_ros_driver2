92fa16e2716d794b039d783e1fda811e5f39b416
George Argyropoulos <gargyropoulos@iknowhow.com>
2025-02-19 15:11:57 +0000

    rsrv into tag filter

--- src/filter_custom.cpp
+++ src/filter_custom.cpp
@@ -54,12 +54,19 @@ namespace livox_ros
             other_high_ = config.other_high;
             other_moderate_ = config.other_moderate;
             other_low_ = config.other_low;
+            other_rsrv_ = config.other_rsrv;
             atm_high_ = config.atm_high;
             atm_moderate_ = config.atm_moderate;
             atm_low_ = config.atm_low;
+            atm_rsrv_ = config.atm_rsrv;
             drag_high_ = config.drag_high;
             drag_moderate_ = config.drag_moderate;
             drag_low_ = config.drag_low;
+            drag_rsrv_ = config.drag_rsrv;
+            rsrv_zero_ = config.rsrv_zero;
+            rsrv_one_ = config.rsrv_one;
+            rsrv_two_ = config.rsrv_two;
+            rsrv_three_ = config.rsrv_three;
         }
 
         void pointCloudCallback(const livox_ros_driver2::CustomMsgConstPtr &cloud_msg)
@@ -111,9 +118,11 @@ namespace livox_ros
                         uint8_t drag_tag = p.tag & 0x03;
                         uint8_t atm_tag = (p.tag >> 2) & 0x03;
                         uint8_t other_tag = (p.tag >> 4) & 0x03;
-                        if (((other_tag == 0 && !other_high_) || (other_tag == 1 && !other_moderate_) || (other_tag == 2 && !other_low_)) ||
-                            ((atm_tag == 0 && !atm_high_) || (atm_tag == 1 && !atm_moderate_) || (atm_tag == 2 && !atm_low_)) ||
-                            ((drag_tag == 0 && !drag_high_) || (drag_tag == 1 && !drag_moderate_) || (drag_tag == 2 && !drag_low_)))
+                        uint8_t reserved_tag = (p.tag >> 6) & 0x03;
+                        if (((other_tag == 0 && !other_high_) || (other_tag == 1 && !other_moderate_) || (other_tag == 2 && !other_low_) || (other_tag == 3 && !other_rsrv_)) ||
+                            ((reserved_tag == 0 && !rsrv_zero_) || (reserved_tag == 1 && !rsrv_one_) || (reserved_tag == 2 && !rsrv_two_) || (reserved_tag == 3 && !rsrv_three_)) ||
+                            ((atm_tag == 0 && !atm_high_) || (atm_tag == 1 && !atm_moderate_) || (atm_tag == 2 && !atm_low_) || (atm_tag == 3 && !atm_rsrv_)) ||
+                            ((drag_tag == 0 && !drag_high_) || (drag_tag == 1 && !drag_moderate_) || (drag_tag == 2 && !drag_low_) || (drag_tag == 3 && !drag_rsrv_)))
                         {
                             return true;
                         }
@@ -130,10 +139,11 @@ namespace livox_ros
 
         boost::recursive_mutex mutex_;
         bool tf_ready_{false};
-        bool enable_box_{true}, enable_tag_{true};        
-        bool atm_high_{true}, atm_moderate_{true}, atm_low_{true};
-        bool drag_high_{true}, drag_moderate_{true}, drag_low_{true};
-        bool other_high_{true}, other_moderate_{true}, other_low_{true};        
+        bool enable_box_{true}, enable_tag_{true};
+        bool rsrv_zero_{true}, rsrv_one_{true}, rsrv_two_{true}, rsrv_three_{true};        
+        bool atm_high_{true}, atm_moderate_{true}, atm_low_{true}, atm_rsrv_{true};
+        bool drag_high_{true}, drag_moderate_{true}, drag_low_{true}, drag_rsrv_{true};
+        bool other_high_{true}, other_moderate_{true}, other_low_{true}, other_rsrv_{true};        
         ros::Publisher pub_;
         ros::Subscriber sub_;      
         std::string box_frame_;
