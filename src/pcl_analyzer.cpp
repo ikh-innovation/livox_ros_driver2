b8d0dfcd3dfc33715976f56e37de607bcee82fbd
Michalis Logothetis <logothm@gmail.com>
2025-05-06 11:46:22 +0300

    support both msg types

--- src/pcl_analyzer.cpp
+++ src/pcl_analyzer.cpp
@@ -2,6 +2,7 @@
 #include <std_msgs/Bool.h>
 #include <nodelet/nodelet.h>
 #include <sensor_msgs/PointCloud2.h>
+#include "livox_ros_driver2/CustomMsg.h"
 #include <pluginlib/class_list_macros.h>
 
 namespace livox_ros
@@ -14,20 +15,42 @@ public:
     {
         ros::NodeHandle& nh = getNodeHandle();
         ros::NodeHandle& nhp = getPrivateNodeHandle();
+
+        // Get type of message to subscribe to
+        int type = nhp.param("msg_type", 0); // 0: PointCloud2, 1: CustomMsg
+        if (type != 0 && type != 1)
+        {
+            NODELET_ERROR("Invalid msg_type parameter. Use 0 for PointCloud2 or 1 for CustomMsg.");
+            return;
+        }
+
+        // Get parameters
         pcl_timeout_ = nhp.param("pcl_topic_timeout", 10.0);
         minimum_points_ = nhp.param("user_few_points_number_threshold", 4000);
         minimum_points_time_ = nhp.param("user_few_points_time_threshold", 10.0);
-        sub_ = nh.subscribe("input_point_cloud", 10, &PclAnalyzerNodelet::pclCallback, this);
+
+        // Publishers
         pub1_ = nh.advertise<std_msgs::Bool>("enough", 10, true);
         pub2_ = nh.advertise<std_msgs::Bool>("active", 10, true);
 
         active_.data = true;
         enough_points_.data = true;
 
+        // Initialize timers
         active_timer_ = nh.createTimer(ros::Duration(pcl_timeout_), &PclAnalyzerNodelet::activeCallback, this);
         enough_timer_ = nh.createTimer(ros::Duration(minimum_points_time_), &PclAnalyzerNodelet::enoughCallback, this);
         active_timer_.start();
-        enough_timer_.start();
+        enough_timer_.start();       
+
+        // Subscribe to the appropriate topic based on the message type
+        if (type == 0)
+        {
+            sub_ = nh.subscribe("input_point_cloud", 10, &PclAnalyzerNodelet::pclCallback, this);
+        }
+        else
+        {
+            sub_ = nh.subscribe("input_point_cloud", 10, &PclAnalyzerNodelet::customCallback, this);
+        }        
     }
 
 private:
@@ -59,6 +82,32 @@ private:
         }
 
         if (msg->width * msg->height > minimum_points_)
+        {
+            enough_timer_.stop();
+            if (!enough_points_.data)
+            {
+                enough_points_.data = true;
+                pub1_.publish(enough_points_);
+            }
+        }
+        else if (!enough_timer_.hasStarted())
+        {
+            enough_timer_.start();
+        }
+        
+        active_timer_.start();
+    }
+
+    void customCallback(const livox_ros_driver2::CustomMsg::ConstPtr& msg)
+    {
+        active_timer_.stop();
+        if (!active_.data)
+        {
+            active_.data = true;
+            pub2_.publish(active_);
+        }
+
+        if (msg->point_num > minimum_points_)
         {
             enough_timer_.stop();
             if (!enough_points_.data)
